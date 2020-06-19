51272cf2f355f339e67a933e7b3238095e04e3a9
thuhole <thuhole@users.noreply.github.com>
2020-06-19 15:57:31 +0000

    200619235729

---
+++ precache-manifest.21ee4d0bc9764302ddc99b9a5753ac05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e9655b0b3076ee9f5c3ca540b4a028",
+    "url": "./index.html"
+  },
+  {
+    "revision": "090e70cd29a83f92213a",
+    "url": "./static/css/main.e52316e2.chunk.css"
+  },
+  {
+    "revision": "8315e3f4741d4388f39f",
+    "url": "./static/js/2.688ae6b0.chunk.js"
+  },
+  {
+    "revision": "090e70cd29a83f92213a",
+    "url": "./static/js/main.5afc1574.chunk.js"
+  },
+  {
+    "revision": "045e50cad03565bb401b",
+    "url": "./static/js/runtime~main.47dca567.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//cdn.jsdelivr.net/gh/thuhole/webhole@gh-pages/precache-manifest.3d3a75d001d12ff90b13b1297d07a85b.js"
+  "./precache-manifest.21ee4d0bc9764302ddc99b9a5753ac05.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//cdn.jsdelivr.net/gh/thuhole/webhole@gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
