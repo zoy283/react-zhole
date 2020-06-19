24bfd5c686e7c41d3154d17962a77f8cfbcad5b7
thuhole <thuhole@protonmail.com>
2020-06-19 10:34:37 +0800

    Updates

---
+++ precache-manifest.52751ac2c82082371a7bc87c012d9bf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382722f9f35ef3834f5d45eeb0253446",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f778a7fa921e57bc8e2",
+    "url": "./static/css/main.54f9c368.chunk.css"
+  },
+  {
+    "revision": "8315e3f4741d4388f39f",
+    "url": "./static/js/2.688ae6b0.chunk.js"
+  },
+  {
+    "revision": "0f778a7fa921e57bc8e2",
+    "url": "./static/js/main.e91b9a8b.chunk.js"
+  },
+  {
+    "revision": "045e50cad03565bb401b",
+    "url": "./static/js/runtime~main.47dca567.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gh/thuhole/thuhole.github.io/precache-manifest.7401cdf895b2828439ad6234dd8767de.js"
+  "./precache-manifest.52751ac2c82082371a7bc87c012d9bf7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gh/thuhole/thuhole.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
