aafc6a8ac0abac3661ba872df920ab4cebee6bc4
Zoy283 <zoy283@qq.com>
2022-05-24 19:49:11 +0800

    第一次适配

---
+++ zhole-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# npm install
+VERSION_NUMBER="v$(grep -oP '"version": "\K[^"]+' package.json | head -n1)" # 确定版本号
+REACT_APP_BUILD_INFO=$VERSION_NUMBER npm run build
+cp -r build ../nginx/
