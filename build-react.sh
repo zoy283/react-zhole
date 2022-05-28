33416e01764efe5779062e1b0af3912be09056bb
Zoy283 <zoy283@qq.com>
2022-05-28 16:27:21 +0800

    去除shell

--- build-react.sh
+++ build-react.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-npm install
+# npm install
 VERSION_NUMBER="v$(grep -oP '"version": "\K[^"]+' package.json | head -n1)" # 确定版本号
 REACT_APP_BUILD_INFO=$VERSION_NUMBER npm run build
 rm -r ../nginx-zhole/build
 cp -r build ../nginx-zhole/
+rm -r ~/test/build
+cp -r build ~/test/
